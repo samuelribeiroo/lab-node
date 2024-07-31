ecfe297a8480a45bd8323d89a2ddd15302076493
samuelribeiroo <samuelribeirodev@gmail.com>
2024-07-31 16:38:21 -0300

    feat(auth): create new user controller

---
+++ src/controllers/auth.ts
@@ -0,0 +1,33 @@
+import type express from "express"
+import { UserModel, createNewUser } from "../database/users"
+import { random } from "../helpers/utils"
+
+export const registerUserController = async (request: express.Request, response: express.Response) => {
+  try {
+    const { username, useremail, password } = request.body
+
+    if (!username || !useremail || !password) {
+      return response.sendStatus(400).end()
+    }
+
+    const isExistAlready = await UserModel.findOne({ useremail })
+
+    if (isExistAlready) {
+      return response.json({ error: "Can't register user. This email alredy exists." }).sendStatus(400)
+    }
+
+    const onCreateNewUser = await createNewUser({
+      username,
+      useremail,
+      authentication: {
+        salt: random(),
+        password,
+      }
+    })
+
+    return response.json(onCreateNewUser).sendStatus(201)
+  } catch (error) {
+    console.log(`There some error on your request: ${error}`)
+    return response.sendStatus(400)
+  }
+}
