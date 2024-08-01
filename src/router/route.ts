053a8976cd931bf566fc913376fe323056aa3a61
samuelribeiroo <samuelribeirodev@gmail.com>
2024-08-01 18:53:02 -0300

    refactor: authentication routes rather than users

--- src/router/route.ts
+++ src/router/route.ts
@@ -8,8 +8,6 @@ router.post("/auth", registerUserController)
 router.post("/auth/:id", loginUserController)
 router.get("/users", getListUsers)
 router.get("/users/:id", getUserById)
-router.post("/users", registerUserController)
-router.post("/users/:id", loginUserController)
 router.delete("/users/:id", deleteUsers)
 
 export default router
