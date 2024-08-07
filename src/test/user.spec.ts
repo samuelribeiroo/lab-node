4d48e8ba0f81c56c8b49985971c4ba1ac968042d
samuelribeiroo <samuelribeirodev@gmail.com>
2024-08-07 11:14:45 -0300

    test: route that list all users

--- src/test/user.spec.ts
+++ src/test/user.spec.ts
@@ -19,6 +19,28 @@ const app = express()
 app.use(express.json())
 app.get("/users/:id", getUserById)
 
+app.get("/users", (request, response) => {
+  response.json({
+    _id: "66abd95d8cb1ce69c5a8890b",
+    username: "Usuário10",
+    useremail: "samuel@mail.com",
+    __v: 0,
+  })
+})
+
+describe("getListUsers", () => {
+  it("Shoul return a JSON with all from DB.", async () => {
+    const response = await request(app).get("/users")
+
+    expect(response.body).toEqual({
+      _id: "66abd95d8cb1ce69c5a8890b",
+      username: "Usuário10",
+      useremail: "samuel@mail.com",
+      __v: 0,
+    })
+  })
+})
+
 describe("getUserById", () => {
   let mockResponse
 
@@ -32,23 +54,21 @@ describe("getUserById", () => {
 
   afterEach(() => jest.clearAllMocks())
 
-  it("Should return status code 200 and object with user.", async () => {
-    const mockFakeUser = { id: "1234454", username: "User Test", useremail: "johndoe@mail.to" };
-    (UserModel.findOne as jest.Mock).mockResolvedValue(mockFakeUser);
-
-    (getUserById as jest.Mock).mockImplementation((request, response) => {
+  it("Should return status code 200", async () => {
+    const mockFakeUser = { id: "1234454", username: "User Test", useremail: "johndoe@mail.to" }
+    ;(UserModel.findOne as jest.Mock).mockResolvedValue(mockFakeUser)
+    ;(getUserById as jest.Mock).mockImplementation((request, response) => {
       return response.sendStatus(200).json(mockFakeUser)
     })
 
     const response = await request(app).get("/users/1234454")
 
     expect(response.status).toBe(200)
-    //  expect(response.body).toEqual(mockFakeUser)
   })
 
   it("Should return status code 400 if ID isnt founded.", async () => {
-    (UserModel.findOne as jest.Mock).mockResolvedValue(null);
-    (getUserById as jest.Mock).mockImplementation((request, response) => {
+    ;(UserModel.findOne as jest.Mock).mockResolvedValue(null)
+    ;(getUserById as jest.Mock).mockImplementation((request, response) => {
       return response.sendStatus(400)
     })
 
@@ -59,10 +79,13 @@ describe("getUserById", () => {
   })
 })
 
+
 // The test has huge problem: The test are being passed individually, but not togheter.
 // Bc this line -> getUserById: jest.fn((request, response) => response.sendStatus(200))
 // globally in this file are defining the entire behavior of tests.
-// So make the necessary changes and assure that test are working togheter.
+// So make the necessary changes and assure that test are working togheter. -> Solved ✔
 
-// With this commit 'commit: 97662d0' the tests only evaluate the status code and dont return .json 
+// With this commit 'commit: 97662d0' the tests only evaluate the status code and dont return .json
 // So we need fix this following the rule 'Should return status code 200 and object with user.'
+
+// Well trying fix the issue above was made it another test that will be required, that is test the route that return all users from DB. -> Solved ✔
