d5f0b8f06c81ca7e1fb8d30e38e73da1bacf729d
Samuel Ribeiro <samuelribeirodev@gmail.com>
2024-09-01 00:11:39 +0000

    refactor: status rather than sendStatus

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -8,17 +8,17 @@ export const registerUserController = async (request: express.Request, response:
     const { username, useremail, password } = request.body
 
     if (!username || !useremail || !password) {
-      return response.sendStatus(400).json({ error: "All fields are required to proceed with registration." })
+      return response.status(400).json({ error: "All fields are required to proceed with registration." })
     }
 
     if (password.length < 8) {
-      return response.json({ error: "Password should have more than 8 characters." }).sendStatus(400)
+      return response.status(400).json({ error: "Password should have more than 8 characters." })
     }
 
     const isExistAlready = await UserModel.findOne({ $or: [{ username }, { useremail }] })
 
     if (isExistAlready) {
-      return response.json({ error: "Can't register user. This email alredy exists." }).sendStatus(400)
+      return response.status(400).json({ error: "Can't register user. This email alredy exists." })
     }
 
     const onCreateNewUser = await createNewUser({
@@ -30,9 +30,9 @@ export const registerUserController = async (request: express.Request, response:
       },
     })
 
-    return response.json(onCreateNewUser).sendStatus(201)
+    return response.status(201).json(onCreateNewUser)
   } catch (error) {
-    return response.sendStatus(400).end()
+    return response.sendStatus(400) 
   }
 }
 
@@ -58,8 +58,8 @@ export const loginUserController = async (request: express.Request, response: ex
       return response.json({ error: "Password or Email incorrect" })
     }
 
-    return response.sendStatus(200).end()
+    return response.sendStatus(200) 
   } catch (error) {
-    return response.sendStatus(400).json({ error: "Login failed." })
+    return response.status(400).json({ error: "Login failed." })
   }
 }
